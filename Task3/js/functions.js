00e8cd494f2fddf73788e71d64b0e5993b258148
MichaelJCini <michaeljcini@hotmail.com>
2019-05-29 12:29:07 +0100

    Finished Task 2 and Task 3

---
+++ Task3/js/functions.js
@@ -0,0 +1,9 @@
+var e = document.getElementById('image');
+e.onmouseover = function() {
+  document.getElementById('popup').classList.toggle("show");
+  document.getElementById('popup').classList.toggle("hide");
+}
+e.onmouseout = function() {
+  document.getElementById('popup').classList.toggle("show");
+  document.getElementById('popup').classList.toggle("hide");
+}
